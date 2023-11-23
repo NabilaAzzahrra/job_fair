ab227d74c69123a8c97262a905688b9463821140
nabilaazzahra <zahrrranabill11@gmail.com>
2023-11-23 14:46:41 +0700

    CompanyOneFiks

--- src/pages/CompanyOne.jsx
+++ src/pages/CompanyOne.jsx
@@ -3,32 +3,31 @@ import React from 'react'
 function CompanyOne() {
     return (
         <div>
-            <section className='bg-red-500 w-full h-[500px]'></section>
+            <section className='bg-white w-full h-[200px]'>
+                <div className="flex justify-center items-center h-full mt-4">
+                    <img src='https://lokertasikmalaya.id/wp-content/uploads/2022/11/TASCO-Mini-Mart.jpeg' alt='Deskripsi gambar' className='object-cover m-6' />
+                </div>
+            </section>
+            {/* <section className='bg-red-300 w-full h-[150px]'>
+                <div className="flex flex-row justify-center items-center">
+                    <div className="flex flex-col">
+                        <img src='src/assets/img/tsc.jpeg' alt='Deskripsi gambar' className='object-cover m-2' />
+                    </div>
+                    <div className="flex flex-col">
+                        <img src='src/assets/img/tsc.jpeg' alt='Deskripsi gambar' className='object-cover m-2' />
+                    </div>
+                    <div className="flex flex-col">
+                        <img src='src/assets/img/tsc.jpeg' alt='Deskripsi gambar' className='object-cover m-2' />
+                    </div>
+                </div>
+            </section> */}
+
             <section className='px-6 py-4'>
                 <div className='flex justify-between items-end'>
                     <div className='space-y-2'>
-                        <h1 className='text-3xl font-bold text-slate-700'>PROBLEM ACCOUNT OFFICER & REMEDIAL OFFICER ALL</h1>
-                        <h1 className='text-2xl text-slate-600'>Location : Wilayah Penempatan Tasik, Banjar dan Garut</h1>
-                        <p className='text-1xl text-slate-500 font-bold'>Salary : ompatitive, dependent on experience & job role</p>
+                        <h1 className='text-3xl font-bold text-slate-700'>ACCOUNTING</h1>
                     </div>
-                    {/* <button className='mt-3 w-40 h-10 bg-blue-600 rounded-lg shadow-md text-white font-bold'>Apply</button> */}
                 </div>
-                <p className='py-4 text-2xl text-slate-700 font-bold'>Job Desk :</p>
-                <p className='text-1xl px-6'>
-                    <ul className='list-disc'>
-                        <li>Menerima daftar kunjungan harian (DKHR) dan melaksanakannya sesuai dengan area yang ditugaskan</li>
-                        <li>Melaksanakan penagihan dan Penerimaa unit penagihan/penerimaan kendaraan atas nasabah yang overdue sesuai bucket didukung oleh proses administrasi yang benar &valid sesuai dengan</li>
-                        <li>Menyerahkan uang hasil penagihan kepada teller dan penerimaan unit kendaraan kepada warehouse dilengkapi dokumen pendukung</li>
-                        <li>Memastikan unit yang diterima merupakan unit objek pembiayaan perusahaan</li>
-                        <li>Menjelaskan dan menawarkan kepada nasabah mengenai Program Rehab/resheduling/Restrukturing serta survey kelayakan nasbah tersebut</li>
-                        <li>Menyusun laporan survey untuk program rehab sesuai dengan kondisi yang sebenarnya dan melampirkan data-data pendukung yang lengkap dan akurat</li>
-                        <li>Mengkomunikasikan pencapaian hasil kerja dan kendala yang dihadapi kepada A/R Head Melaporkan Hasil kunjungan den mengkomunikasikan kesulitan yang ditemui saat kunjungan kepada atasan</li>
-                    </ul>
-                </p>
-                <p className='py-4 text-2xl text-slate-700 font-bold'>Kebutuhan :</p>
-                <p className='text-1xl'>
-                    6 Orang Tasikmalaya, 6 Garut,3 Banjar Wilayah penempatan domili Ciamis, Banjar, Pangandaran, Tasikmalaya, Singaparna, Karangnunggal, Garut, Limbangan , Cikajang
-                </p>
                 <p className='py-4 text-2xl text-slate-700 font-bold'>Kriteria Kandidat :</p>
                 <p className='px-1'>
                     <ul className='space-y-4 text-left text-gray-500 dark:text-gray-400'>
@@ -36,45 +35,160 @@ function CompanyOne() {
                             <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            <span>Laki- Laki Data Admin Staff</span>
+                            <span>Jenis Kelamin Laki/laki - Perempuan</span>
                         </li>
                         <li className='flex items-center space-x-3 rtl:space-x-reverse'>
                             <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            <span>Usia Max 28 tahun</span>
+                            <span>Fresh graduate/ Pengalaman lebih diutamakan</span>
                         </li>
                         <li className='flex items-center space-x-3 rtl:space-x-reverse'>
                             <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            <span>Menyukai kerja Lapangan</span>
+                            <span>Usia maksimal 25 tahun</span>
                         </li>
                         <li className='flex items-center space-x-3 rtl:space-x-reverse'>
                             <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            <span>Memiliki Kendaraan Roda</span>
+                            <span>Pendidikan D3/ S1 Semua jurusan</span>
                         </li>
                         <li className='flex items-center space-x-3 rtl:space-x-reverse'>
                             <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            <span>Menyukai Tantangan</span>
+                            <span>Mampu bekerja secara individu atau team</span>
                         </li>
                         <li className='flex items-center space-x-3 rtl:space-x-reverse'>
                             <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            <span>IPK minimal 2,75</span>
+                            <span>Mampu bekerja dibawah tekanan, disiplin, gesit, jujur dan tanggung jawab</span>
                         </li>
                     </ul>
                 </p>
-                <a href='https://flowbite.com/docs/components/buttons/' className='mt-3 flex'>
+                <a href='https://forms.gle/24Kb7swwaCDWVW5m7' className='mt-3 flex'>
                     <span className='flex items-center justify-center w-40 h-10 bg-blue-600  shadow-md text-white font-bold'> Apply Now </span>
                     <span className='flex items-center justify-center w-10 h-10 bg-blue-700  shadow-md text-white font-bold'><i className="fa-solid fa-caret-right"></i></span>
                 </a>
             </section>
+
+            <div>
+                <hr className='my-5 border-t-2 m-6 border-cyan-700'></hr>
+            </div>
+
+            <section className='px-6 py-4'>
+                <div className='flex justify-between items-end'>
+                    <div className='space-y-2'>
+                        <h1 className='text-3xl font-bold text-slate-700'>MARKETING</h1>
+                    </div>
+                </div>
+                <p className='py-4 text-2xl text-slate-700 font-bold'>Kriteria Kandidat :</p>
+                <p className='px-1'>
+                    <ul className='space-y-4 text-left text-gray-500 dark:text-gray-400'>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Jenis Kelamin Laki/laki - Perempuan</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Fresh graduate/ Pengalaman lebih diutamakan</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Usia maksimal 25 tahun</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Pendidikan D3/ S1 Semua jurusan</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Mampu bekerja secara individu atau team</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Mampu bekerja dibawah tekanan, disiplin, gesit, jujur dan tanggung jawab</span>
+                        </li>
+                    </ul>
+                </p>
+                <a href='https://forms.gle/24Kb7swwaCDWVW5m7' className='mt-3 flex'>
+                    <span className='flex items-center justify-center w-40 h-10 bg-blue-600  shadow-md text-white font-bold'> Apply Now </span>
+                    <span className='flex items-center justify-center w-10 h-10 bg-blue-700  shadow-md text-white font-bold'><i className="fa-solid fa-caret-right"></i></span>
+                </a>
+            </section>
+
+            <div>
+                <hr className='my-5 border-t-2 m-6 border-cyan-700'></hr>
+            </div>
+
+            <section className='px-6 py-4'>
+                <div className='flex justify-between items-end'>
+                    <div className='space-y-2'>
+                        <h1 className='text-3xl font-bold text-slate-700'>WAKIL KEPALA TOKO</h1>
+                    </div>
+                </div>
+                <p className='py-4 text-2xl text-slate-700 font-bold'>Kriteria Kandidat :</p>
+                <p className='px-1'>
+                    <ul className='space-y-4 text-left text-gray-500 dark:text-gray-400'>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Jenis Kelamin Laki/laki - Perempuan</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Fresh graduate/ Pengalaman lebih diutamakan</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Usia maksimal 25 tahun</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Pendidikan D3/ S1 Semua jurusan</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Mampu bekerja secara individu atau team</span>
+                        </li>
+                        <li className='flex items-center space-x-3 rtl:space-x-reverse'>
+                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                            <span>Mampu bekerja dibawah tekanan, disiplin, gesit, jujur dan tanggung jawab</span>
+                        </li>
+                    </ul>
+                </p>
+                <a href='https://forms.gle/24Kb7swwaCDWVW5m7' className='mt-3 flex'>
+                    <span className='flex items-center justify-center w-40 h-10 bg-blue-600  shadow-md text-white font-bold'> Apply Now </span>
+                    <span className='flex items-center justify-center w-10 h-10 bg-blue-700  shadow-md text-white font-bold'><i className="fa-solid fa-caret-right"></i></span>
+                </a>
+            </section>
+
         </div>
     )
 }
